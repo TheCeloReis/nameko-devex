0e85a0d95a4c3d2143471ecfdc6e6d6efa4522d9
Celo Reis <hello@celoreis.dev>
2023-04-03 12:40:55 -0300

    feat: theme configuration

--- gateapi/web/tailwind.config.cjs
+++ gateapi/web/tailwind.config.cjs
@@ -1,7 +1,20 @@
 module.exports = {
 	content: ['./src/**/*.html', './src/**/*.svelte'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['Amplitude Wide', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				text: '#35344C',
+				background: '#FCFCFD',
+
+				primary: '#3F25C8',
+				'primary-light': '#474374',
+				'primary-lighter': '#C3C1D7',
+				'primary-lightest': '#F3F1FE',
+			},
+		},
 	},
 	plugins: [],
 }
